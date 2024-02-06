5d0d0b530e89dc0a5e45ca6a72c4b3d2ca9a1675
JiyunJeong01 <wjdwldbsskfk@gmail.com>
2024-02-06 18:41:15 +0900

    Query Method란 무엇일까?

--- src/main/java/com/sparta/memo/service/MemoService.java
+++ src/main/java/com/sparta/memo/service/MemoService.java
@@ -4,12 +4,12 @@ import com.sparta.memo.dto.MemoRequestDto;
 import com.sparta.memo.dto.MemoResponseDto;
 import com.sparta.memo.entity.Memo;
 import com.sparta.memo.repository.MemoRepository;
-import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 
-@Component
+@Service
 public class MemoService {
 
     private final MemoRepository memoRepository;
@@ -26,29 +26,30 @@ public class MemoService {
         Memo saveMemo = memoRepository.save(memo);
 
         // Entity -> ResponseDto
-        MemoResponseDto memoResponseDto = new MemoResponseDto(memo);
+        MemoResponseDto memoResponseDto = new MemoResponseDto(saveMemo);
 
         return memoResponseDto;
     }
 
     public List<MemoResponseDto> getMemos() {
         // DB 조회
-        return memoRepository.findAll().stream().map(MemoResponseDto::new).toList();
+        return memoRepository.findAllByOrderByModifiedAtDesc().stream().map(MemoResponseDto::new).toList();
     }
 
     @Transactional
     public Long updateMemo(Long id, MemoRequestDto requestDto) {
         // 해당 메모가 DB에 존재하는지 확인
-        Memo memo = fineMemo(id);
+        Memo memo = findMemo(id);
 
         // memo 내용 수정
         memo.update(requestDto);
+
         return id;
     }
 
     public Long deleteMemo(Long id) {
         // 해당 메모가 DB에 존재하는지 확인
-        Memo memo = fineMemo(id);
+        Memo memo = findMemo(id);
 
         // memo 삭제
         memoRepository.delete(memo);
@@ -56,7 +57,7 @@ public class MemoService {
         return id;
     }
 
-    private Memo fineMemo(Long id) {
+    private Memo findMemo(Long id) {
         return memoRepository.findById(id).orElseThrow(() ->
                 new IllegalArgumentException("선택한 메모는 존재하지 않습니다.")
         );
